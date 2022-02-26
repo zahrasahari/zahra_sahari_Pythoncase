5a97f1d49ab274c7531b9525f713f8cdb59d1c7a
zahrasahari <100036603+zahrasahari@users.noreply.github.com>
2022-02-26 21:24:11 +0330

    Update HW2-6_zahrasahari.py

--- HW2-6_zahrasahari.py
+++ HW2-6_zahrasahari.py
@@ -1,6 +1,13 @@
-print("2");  #HW2-6-a
-print('2');  #HW2-6-b
-print(2);    #HW2-6-c
-print("2 + 1");  #HW2-6-d  
-print('2' + 1);  #HW2-6-e
-print(2 + 1);    #HW2-2-f
+x = 2 
+
+print("x")
+
+print('x')
+
+print(x)
+
+print("x+1")
+
+print('x+1')
+
+print(x+1)
