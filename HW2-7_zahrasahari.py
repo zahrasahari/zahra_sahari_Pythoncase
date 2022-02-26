3bd3f0de50f9bce8d455c84c2a08440f3b47bfef
zahrasahari <100036603+zahrasahari@users.noreply.github.com>
2022-02-26 19:06:56 +0330

    Create HW2-7_zahrasahari.py

---
+++ HW2-7_zahrasahari.py
@@ -0,0 +1,30 @@
+#HW-2-7
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+print (i1 + i2);                #HW2-7-a
+print (i1 / i2);                #HW2-7-b
+print (i1 // i2);               #HW2-7-c
+print (i2 / i1);                #HW2-7-d
+print (i2 // i1);               #HW2-7-e
+print (i1 * i3);                #HW2-7-f
+print (d1 + d2);                #HW2-7-g
+print (d1 / d2);                #HW2-7-h
+print (d2 / d1);                #HW2-7-i
+print (d3 * d1);                #HW2-7-j
+print (d1 + i2);                #HW2-7-k
+print (i1 / d2);                #HW2-7-l
+print (d2 / i1);                #HW2-7-m
+print (i2 / d1);                #HW2-7-n
+print (i1/i2*d1);               #HW2-7-o
+print (d1*i1/i2);               #HW2-7-p
+print (d1/d2*i1);               #HW2-7-q
+print (i1*d1/d2);               #HW2-7-r
+print (i2/i1*d1);               #HW2-7-s
+print (d1*i2/i1);               #HW2-7-t
+print (d2/d1*i1);               #HW2-7-u
+print (i1*d2/d1);               #HW2-7-v
+
