d74b13862af055f77a2e22cc7eabb67404cdfabe
zahrasahari <100036603+zahrasahari@users.noreply.github.com>
2022-03-04 16:33:40 +0330

    Create HW3-1_zahrasahari.py
     سلام . خروجی برنامه قسمت اول تمرین اول
    👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻
    i = 5 j = 5 k = 7

---
+++ HW3-1_zahrasahari.py
@@ -0,0 +1,15 @@
+i , j , k = 3 , 5 , 7
+if i < j:
+    if i < k:
+        i = j 
+    else :
+        i = k 
+        
+        
+else:
+    if i > k:
+        j = i 
+    else :
+        i = k 
+        
+print("i =" , i, "j =" , j,"k =" , k)    
