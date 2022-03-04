2aa516ca3f3fec60774fb9336ab083d37bf245e3
zahrasahari <100036603+zahrasahari@users.noreply.github.com>
2022-03-04 17:29:15 +0330

    Create HW3-2_zahrasahari.py

---
+++ HW3-2_zahrasahari.py
@@ -0,0 +1,100 @@
+#Homework 3 - 2 - part a
+val = 3
+if val < 10 :
+    if val != 5 :
+        print (" Wow " , end= ' ')
+        
+    else:
+        val += 1
+        
+        
+else :
+    if val == 17:
+       val += 10 
+    else:
+        print (" Whoa " , end= ' ')
+        
+        
+print (val) 
+#result : Wow  3
+
+#Homework 3 - 2 - part b
+val = 21
+if val < 10 :
+    if val != 5 :
+        print (" Wow " , end= ' ')
+        
+    else:
+        val += 1
+        
+        
+else :
+    if val == 17:
+       val += 10 
+    else:
+        print (" Whoa " , end= ' ')
+        
+        
+print (val) 
+#result :  Whoa  21
+
+#Homework 3 - 2 - part c
+val = 5
+if val < 10 :
+    if val != 5 :
+        print (" Wow " , end= ' ')
+        
+    else:
+        val += 1
+        
+        
+else :
+    if val == 17:
+       val += 10 
+    else:
+        print (" Whoa " , end= ' ')
+        
+        
+print (val) 
+#result : 6
+
+#Homework 3 - 2 - part d
+val = 17
+if val < 10 :
+    if val != 5 :
+        print (" Wow " , end= ' ')
+        
+    else:
+        val += 1
+        
+        
+else :
+    if val == 17:
+       val += 10 
+    else:
+        print (" Whoa " , end= ' ')
+        
+        
+print (val) 
+#result : 27
+
+#Homework 3 - 2 - part e
+val = -5
+if val < 10 :
+    if val != 5 :
+        print (" Wow " , end= ' ')
+        
+    else:
+        val += 1
+        
+        
+else :
+    if val == 17:
+       val += 10 
+    else:
+        print (" Whoa " , end= ' ')
+        
+        
+print (val) 
+#result :  Wow  -5
+
