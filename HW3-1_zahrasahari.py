be9c5c3c1b36077d183966ea6be0556512082f7b
zahrasahari <100036603+zahrasahari@users.noreply.github.com>
2022-03-04 16:59:20 +0330

    Update HW3-1_zahrasahari.py

--- HW3-1_zahrasahari.py
+++ HW3-1_zahrasahari.py
@@ -1,3 +1,5 @@
+#Homework 3 - 1 - part a
+
 i , j , k = 3 , 5 , 7
 if i < j:
     if i < k:
@@ -12,4 +14,106 @@ else:
     else :
         i = k 
         
-print("i =" , i, "j =" , j,"k =" , k)    
+print("i =" , i, "j =" , j,"k =" , k)
+
+#result : i = 5 j = 5 k = 7
+
+#Homework 3 - 1 - part b
+
+i , j , k = 3 , 7 , 5
+if i < j:
+    if i < k:
+        i = j 
+    else :
+        i = k 
+        
+        
+else:
+    if i > k:
+        j = i 
+    else :
+        i = k 
+        
+print("i =" , i, "j =" , j,"k =" , k)
+
+#result : i = 7 j = 7 k = 5
+
+#Homework 3 - 1 - part c
+i , j , k = 5 , 3 , 7
+if i < j:
+    if i < k:
+        i = j 
+    else :
+        i = k 
+        
+        
+else:
+    if i > k:
+        j = i 
+    else :
+        i = k 
+        
+print("i =" , i, "j =" , j,"k =" , k)
+
+#result : i = 7 j = 3 k = 7
+
+#Homework 3 - 1 - part d
+
+i , j , k = 5 , 7 , 3
+if i < j:
+    if i < k:
+        i = j 
+    else :
+        i = k 
+        
+        
+else:
+    if i > k:
+        j = i 
+    else :
+        i = k 
+        
+print("i =" , i, "j =" , j,"k =" , k)
+
+#result : i = 3 j = 7 k = 3
+
+#Homework 3 - 1 - part e
+
+i , j , k = 7 , 3 , 5
+if i < j:
+    if i < k:
+        i = j 
+    else :
+        i = k 
+        
+        
+else:
+    if i > k:
+        j = i 
+    else :
+        i = k 
+        
+print("i =" , i, "j =" , j,"k =" , k)
+
+#result : i = 7 j = 7 k = 5
+
+#Homework 3 - 1 - part f
+
+i , j , k = 7 , 5 , 3
+if i < j:
+    if i < k:
+        i = j 
+    else :
+        i = k 
+        
+        
+else:
+    if i > k:
+        j = i 
+    else :
+        i = k 
+        
+print("i =" , i, "j =" , j,"k =" , k)
+
+#result : i = 7 j = 7 k = 3
+
