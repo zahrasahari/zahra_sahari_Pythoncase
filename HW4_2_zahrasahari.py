f7e1bc3293e24288675ba27a16e167d0b63050fa
zahrasahari <100036603+zahrasahari@users.noreply.github.com>
2022-03-13 18:22:01 +0330

    Create HW4_2_zahrasahari.py

---
+++ HW4_2_zahrasahari.py
@@ -0,0 +1,13 @@
+#second HW
+MAX = 20
+n = 1
+while n <= MAX:
+    factor = 1
+    print (end=str(n)+':')
+    while factor <=n:
+        if n % factor == 0:
+            print (factor , end = '')
+            factor += 1 
+            print ()
+            n += 1
+            break 
