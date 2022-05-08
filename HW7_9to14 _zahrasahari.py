03b845a6717ada2732bfe3a266f8a7abac637bc0
zahrasahari <100036603+zahrasahari@users.noreply.github.com>
2022-05-08 22:09:26 +0430

    Create HW7_9to14 _zahrasahari.py

---
+++ HW7_9to14 _zahrasahari.py
@@ -0,0 +1,31 @@
+#HW_9
+(A)lst[0:5]=[2,4,6,8,10]
+(B)
+(C)
+(D)lst[3:4]=['a','b','c']
+(E)lst[ : ]
+(F)
+(G)lst[-1: ]
+(H)lst[ :3]
+(I)lst[2: ]
+(J)lst[1:4]=[ ]
+(K)lst[1:4]
+#HW_10
+a [8,8,8,8]     ///// b [2,7,2,7,2,7,2,7,2,7,2,7]  /////  c [1,2,3,'a','b','c','d']
+d [1,2,1,2,1,2,4,2]
+e [1,2,4,2,1,2,4,2,1,2,4,2]
+#HW_11
+a [3,5,7,9]
+b [50,60,70,80,90]
+c [12,15,18]
+d [(0,0),(0,1),(0,2),(0,3),(1,0),(1,1),(1,2),(1,3),(2,0),(2,1),(2,2),(2,3)]
+e [(0,0),(0,2),(1,1),(1,3),(2,0),(2,2)]
+#HW_12
+(a) [x**2 for x in range (1,6)]
+(b) [x/4 for x in range (1,7)]
+(c) [(x,y) for x in ['B','b'] for y in [0,1,2]]
+#HW_13
+x in lst (show existence)
+x not in lst (show not existence)
+#HW_14
+reverse()function is used to reverse the order of objects in a list data structure in place.
