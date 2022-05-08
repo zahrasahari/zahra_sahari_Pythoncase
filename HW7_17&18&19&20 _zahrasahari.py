84f2cab3d3d24028a4ca5dc0356688d01cb9d51d
zahrasahari <100036603+zahrasahari@users.noreply.github.com>
2022-05-08 23:25:58 +0430

    Update HW7_17&18&19&20 _zahrasahari.py

--- HW7_17&18&19&20 _zahrasahari.py
+++ HW7_17&18&19&20 _zahrasahari.py
@@ -1,42 +1,39 @@
 #HW_17
-def bigenough(lis):
-    x = int(input("Enter a number as acompare:"))
-    jadid=[]
-    for i in lis:
-        if  i>=x:
-            jadid += [i]
-    return jadid
-    
-lis=[]
-for i in range (0,4):
-    n=int(input("Enter a number of list:"))
-    lis+=[n]
+def print_big_enough(lst, x):
+    for i in lst:
+        if i >= x:
+            print(i)
     
 /////////////////////////////////////////////////////////////////////////////////    
 #HW_18
 def next_number(lst):
-    mymax = 0
-    num = 0
-    for i in lst:
-        if i > mymax:
-            mymax = i
-
-    for i in range(1, mymax + 1):
-        if i not in lst:
-            return i
-
-    return mymax + 1
-
-
-print(next_number(lst))
+    positive_int = 1
+    while 1:
+        if positive_int not in lst:
+            return positive_int
+        positive_int += 1
 //////////////////////////////////////////////////////////////////////////////////
 #HW_19
-def reverse(lis):
-    jadid=[]
-    for i in lis[ ::-1]:
-            jadid += [i]
-    return jadid
+def reverse(lst):
+    reversed_list = [0 for i in range(len(lst))]
+
+    for i in range(len(lst)):
+        reversed_list[len(lst) - i - 1] = lst[i]
+
+    return reversed_list
 //////////////////////////////////////////////////////////////////////////////////
 #HW_20
+ m = [[1 for i in range(9)] for j in range(6)]
+    
+    for i in range(6):
+        for j in range(9):
+            print(m[i][j], end='  ')
+        print('\n')
+    print('\n\n')
+    m[2][4] = 0
 
+    for i in range(6):
+        for j in range(9):
+            print(m[i][j], end='  ')
+        print('\n')
 
